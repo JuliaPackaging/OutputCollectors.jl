949530176caef159553f46bc53c2aca286c4a15c
Mosè Giordano <mose@gnu.org>
2020-06-06 20:40:59 +0100

    Add more tests

--- test/command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "Print to stdout"
-echo "Print to stderr" > /dev/stderr
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,26 @@ using OutputCollectors
 using Test
 
 @testset "OutputCollectors.jl" begin
+    @test_logs (:warn, r"Could not spawn") @test_throws Base.IOError OutputCollector(`does not exist`)
+
     if Sys.isunix()
-        cmd = joinpath(@__DIR__, "command.sh")
-        oc = OutputCollector(`$cmd`)
-        @test merge(oc) == "Print to stdout\nPrint to stderr\n"
+        oc = OutputCollector(`echo`; verbose = true)
+        @test merge(oc) == "\n"
+
+        stdout_msg = "Print to stdout"
+        stderr_msg = "Print to stderr"
+        cmd = """
+              echo $(stdout_msg)
+              echo $(stderr_msg) > /dev/stderr
+              """
+        oc = OutputCollector(`bash -c $cmd`)
+        @test merge(oc) == stdout_msg * "\n" * stderr_msg * "\n"
         @test merge(oc; colored = true) ==
-            "Print to stdout\n" * Base.text_colors[:red] * "Print to stderr\n" * Base.text_colors[:default]
+            stdout_msg * "\n" * Base.text_colors[:red] * stderr_msg * "\n" * Base.text_colors[:default]
+        @test tail(oc; len = 1) == stderr_msg * "\n"
+        @test tail(oc; len = 2, colored = true) ==
+            Base.text_colors[:red] * stderr_msg * "\n" * Base.text_colors[:default]
+        @test collect_stdout(oc) == stdout_msg * "\n"
+        @test collect_stderr(oc) == stderr_msg * "\n"
     end
 end
